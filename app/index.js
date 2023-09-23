3601ca16c62a6cb0c810b30c4662c8b90fcf86cd
gabrielmj23 <gabrielmj2312@gmail.com>
2023-09-23 13:24:56 -0400

    Cambiar a expo router y actualizar linter

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Index () {
+  return <Text className='bg-slate-500'>Hola mundo</Text>
+}
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -12,12 +11,11 @@ const firebaseConfig = {
   storageBucket: 'tutor-plus-ucab.appspot.com',
   messagingSenderId: '265559781258',
   appId: '1:265559781258:web:559d5d7a4a6b559b58e273',
-  measurementId: 'G-5S76EXB0ZN',
-};
+  measurementId: 'G-5S76EXB0ZN'
+}
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
