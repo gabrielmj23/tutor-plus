a02537f4717b5d2f0c5b34a54a36c4702ee29753
Andres Roberto Chacon Donoso <109924526+AndresChacon00@users.noreply.github.com>
2023-09-23 22:09:58 -0400

    pantalla Administrar Tutoria

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Text } from 'react-native'
+import AdministrarTutoriaIndex from './administrar-tutoria/index.js'
+
+export default function Index () {
+  return <AdministrarTutoriaIndex />
+}
